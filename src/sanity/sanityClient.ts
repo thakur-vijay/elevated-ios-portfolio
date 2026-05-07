361f559427b2aabc2cb5ca5b6f968c37fce2f6c5
ThakurVijay210901 <105584645+ThakurVijay2191@users.noreply.github.com>
2026-05-07 12:53:10 +0530

    refactor: specify API version in Sanity client configuration

--- src/sanity/sanityClient.ts
+++ src/sanity/sanityClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export default createClient({
+  apiVersion: "2024-06-01", // use a specific API version
   projectId: "te8jbj37", // ✅ Replace with your actual project ID
   dataset: "production", // use a specific API version
   useCdn: true, // `false` if you want fresh data
